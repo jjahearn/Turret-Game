906881e5975db7a763abd7af3b6faa073f3b1bdd
jjahearn <j.j.ahearn@gmail.com>
2017-10-20 13:38:06 -0400

    more 3d testing

--- core/src/com/mygdx/game/screens/BattleScreen.java
+++ core/src/com/mygdx/game/screens/BattleScreen.java
@@ -11,6 +11,7 @@ import com.badlogic.gdx.graphics.g3d.Model;
 import com.badlogic.gdx.graphics.g3d.ModelInstance;
 import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
 import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
+import com.badlogic.gdx.graphics.g3d.utils.CameraInputController;
 import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
 import com.badlogic.gdx.math.Matrix4;
 import com.mygdx.game.TurretGame;
@@ -27,14 +28,20 @@ public class BattleScreen implements Screen {
     private Model model;
     private ModelInstance testBox;
     private Environment environment;
-
     private PerspectiveCamera camera;
+    private CameraInputController cameraControl;
 
     public BattleScreen(TurretGame game){
         this.game = game;
         debugView = new Debug(game.spriteBatch);
         CameraSetup();
         CreateTestBox();
+        BuildEnvironment();
+        cameraControl = new CameraInputController(camera);
+        Gdx.input.setInputProcessor(cameraControl);
+    }
+
+    private void BuildEnvironment() {
         environment = new Environment();
         environment.set(new ColorAttribute(ColorAttribute.AmbientLight, 0.4f, 0.4f, 0.4f, 1f));
         environment.add(new DirectionalLight().set(0.8f, 0.8f, 0.8f, -1f, -0.8f, -0.2f));
@@ -50,7 +57,7 @@ public class BattleScreen implements Screen {
 
     private void CameraSetup() {
         camera = new PerspectiveCamera(67, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-        camera.position.set(10f, 10f, 10f);
+        camera.position.set(0f, 0f, 10f);
         camera.lookAt(0,0,0);
         camera.near = 1f;
         camera.far = 300f;
@@ -66,6 +73,7 @@ public class BattleScreen implements Screen {
     public void render(float delta) {
         update(delta);
 
+        Gdx.gl.glClearColor(0,0,0,1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
 
         game.modelBatch.begin(camera);
@@ -81,6 +89,8 @@ public class BattleScreen implements Screen {
     }
 
     private void update(float delta){
+
+        testBox.transform.rotate(0f,5f, 0f, 20f*delta);
 
         if (game.debug){
             debugView.update();
--- core/src/com/mygdx/game/screens/WorldScreen.java
+++ core/src/com/mygdx/game/screens/WorldScreen.java
@@ -75,6 +75,7 @@ public class WorldScreen implements Screen {
         //game logic
         update(delta);
 
+        Gdx.gl.glClearColor(0,0,0,1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         //draw things
